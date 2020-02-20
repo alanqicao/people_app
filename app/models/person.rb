e43dc145177539037e3d36bfd9949bb7225e7525
Qi Cao <alanqicao@gmail.com>
2020-02-19 23:16:03 -0700

    fix modle

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,15 +1,15 @@
 class Person < ApplicationRecord
   
-  def alive(:alive)
-    if alive=true
-      puts 'Yes'
-    else
-      puts 'No'
-    end
-  end
+  # def alive(:alive)
+  #   if alive=true
+  #     puts 'Yes'
+  #   else
+  #     puts 'No'
+  #   end
+  # end
 
-  def full_name(name)
-    @full_name = 
-  end
+  # def full_name(name)
+  #   @full_name = 
+  # end
     
 end
